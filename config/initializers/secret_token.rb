a8ed8e26f9b846c6f63be2acf69769dcf01f3b3a
Bui Thanh Hai <hai.bth@gmail.com>
2012-12-13 16:17:37 +0700

    Init the OneCheck app with basic information

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onecheck::Application.config.secret_token = '36c6a5c33d327abab4913272a1cf7d6d16f7201282ba7c874e92990628f98a142260ee8458f71ab21fe30036d203e6bfd0514ea87f0ea6e6b94b86c7cc863429'
