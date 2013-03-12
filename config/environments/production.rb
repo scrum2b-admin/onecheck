28b83c41fb94b4b95d180dea3715c680f812ac84
Tuan Ho Anh <tuanha@ithanoi.com>
2013-03-12 18:54:59 +0700

    add config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,7 @@ Onecheck::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  config.assets.precompile += %w( bootstrap.min.css )
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
